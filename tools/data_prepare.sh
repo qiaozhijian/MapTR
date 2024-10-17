8105163bbee5ef2bca70fe54757da738b35b29e3
qiaozhijian <935514540@qq.com>
2024-10-17 11:50:48 +0800

    update

--- tools/data_prepare.sh
+++ tools/data_prepare.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=4
 
 #DATA_ROOT=/home/qzj/datasets/nuscenes
 #python tools/maptrv2/custom_nusc_map_converter.py --root-path ${DATA_ROOT} --out-dir ${DATA_ROOT}/custom/pkls/  --canbus ${DATA_ROOT}
@@ -9,12 +9,12 @@ export CUDA_VISIBLE_DEVICES=3
 export ROOT_DIR=$PWD
 export PYTHONPATH=$ROOT_DIR:$PYTHONPATH
 export GPUS=1
-export PORT=${PORT:-29504}
+export PORT=${PORT:-29505}
 
-export CONFIG=/home/qzj/code/MapTR/projects/configs/maptrv2/maptrv2_nusc_r50_24ep_qzj.py
-export CHECKPOINT=/home/qzj/code/MapTR/ckpts/maptrv2_nusc_r50_24e.pth
-python3 -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT tools/test.py $CONFIG $CHECKPOINT --launcher pytorch --eval chamfer
-
-#export CONFIG=/home/qzj/code/MapTR/projects/configs/maptrv2/maptrv2_nusc_r50_24ep_qzj2.py
+#export CONFIG=/home/qzj/code/MapTR/projects/configs/maptrv2/maptrv2_nusc_r50_24ep_qzj.py
 #export CHECKPOINT=/home/qzj/code/MapTR/ckpts/maptrv2_nusc_r50_24e.pth
 #python3 -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT tools/test.py $CONFIG $CHECKPOINT --launcher pytorch --eval chamfer
+
+export CONFIG=/home/qzj/code/MapTR/projects/configs/maptrv2/maptrv2_nusc_r50_24ep_qzj2.py
+export CHECKPOINT=/home/qzj/code/MapTR/ckpts/maptrv2_nusc_r50_24e.pth
+python3 -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT tools/test.py $CONFIG $CHECKPOINT --launcher pytorch --eval chamfer
