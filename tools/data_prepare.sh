1859bb6adb2c2b5fade104bd68f209a7630d9afa
qiaozhijian <935514540@qq.com>
2024-10-17 11:50:08 +0800

    update

--- tools/data_prepare.sh
+++ tools/data_prepare.sh
@@ -1,20 +1,20 @@
-CUDA_VISIBLE_DEVICES=4
+export CUDA_VISIBLE_DEVICES=3
 
-DATA_ROOT=/home/qzj/datasets/nuscenes
-python tools/maptrv2/custom_nusc_map_converter.py --root-path ${DATA_ROOT} --out-dir ${DATA_ROOT}/custom/pkls/  --canbus ${DATA_ROOT}
-python tools/maptrv2/custom_nusc_map_converter.py --root-path ${DATA_ROOT} --out-dir ${DATA_ROOT}/custom/pkls2/ --canbus ${DATA_ROOT} --keyframe
+#DATA_ROOT=/home/qzj/datasets/nuscenes
+#python tools/maptrv2/custom_nusc_map_converter.py --root-path ${DATA_ROOT} --out-dir ${DATA_ROOT}/custom/pkls/  --canbus ${DATA_ROOT}
+#python tools/maptrv2/custom_nusc_map_converter.py --root-path ${DATA_ROOT} --out-dir ${DATA_ROOT}/custom/pkls2/ --canbus ${DATA_ROOT} --keyframe
+#
+#python tools/maptrv2/custom_av2_map_converter.py --data-root /home/qzj/datasets/argoverse2/sensor/
 
-python tools/maptrv2/custom_av2_map_converter.py --data-root /home/qzj/datasets/argoverse2/sensor/
+export ROOT_DIR=$PWD
+export PYTHONPATH=$ROOT_DIR:$PYTHONPATH
+export GPUS=1
+export PORT=${PORT:-29504}
 
-ROOT_DIR=$PWD
-PYTHONPATH=$ROOT_DIR:$PYTHONPATH
-GPUS=1
-PORT=${PORT:-29505}
-
-CONFIG=/home/qzj/code/MapTR/projects/configs/maptrv2/maptrv2_nusc_r50_24ep_qzj.py
-CHECKPOINT=/home/qzj/code/MapTR/ckpts/maptrv2_nusc_r50_24e.pth
+export CONFIG=/home/qzj/code/MapTR/projects/configs/maptrv2/maptrv2_nusc_r50_24ep_qzj.py
+export CHECKPOINT=/home/qzj/code/MapTR/ckpts/maptrv2_nusc_r50_24e.pth
 python3 -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT tools/test.py $CONFIG $CHECKPOINT --launcher pytorch --eval chamfer
 
-#CONFIG=/home/qzj/code/MapTR/projects/configs/maptrv2/maptrv2_nusc_r50_24ep_qzj.py
-#CHECKPOINT=/home/qzj/code/MapTR/ckpts/maptrv2_nusc_r50_24e.pth
+#export CONFIG=/home/qzj/code/MapTR/projects/configs/maptrv2/maptrv2_nusc_r50_24ep_qzj2.py
+#export CHECKPOINT=/home/qzj/code/MapTR/ckpts/maptrv2_nusc_r50_24e.pth
 #python3 -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT tools/test.py $CONFIG $CHECKPOINT --launcher pytorch --eval chamfer
